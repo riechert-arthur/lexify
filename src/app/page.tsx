0fd927aefdb7c5c565b9d365de649081fdbca177
Arthur <arthur@cybrco.ai>
2024-11-26 01:10:35 -0500

    initial commit

---
+++ src/app/page.tsx
@@ -0,0 +1,13 @@
+import Image from "next/image";
+
+import { type FC } from "react"
+
+interface HomePageProps {}
+
+const HomePage: FC<HomePageProps> = () => {
+  return (
+    <main></main> 
+  )
+}
+
+export default HomePage
